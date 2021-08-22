906b9a461e1e3e4811da091eb286dfa7c7ed1011
Marijan Brvar <marijan.brvar@gmail.com>
2021-08-23 01:01:53 +0200

    Add initial stubs for components

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,15 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+
+class TodoContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from Create React Ap</h1>
+        <p>I am in a React Component!</p>
+      </div>
+    );
+  }
+}
+
+export default TodoContainer;
