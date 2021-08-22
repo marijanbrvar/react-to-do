97c6b58c5c33cc1f8b80a01bd24ca2deb61bf3a0
Marijan Brvar <marijan.brvar@gmail.com>
2021-08-23 01:17:22 +0200

    Add some data to todo list

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,13 +1,37 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/no-unused-state */
+/* eslint-disable react/state-in-constructor */
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 
 class TodoContainer extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Setup development environment',
+        completed: true,
+      },
+      {
+        id: 2,
+        title: 'Develop website and add content',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'Deploy to live server',
+        completed: false,
+      },
+    ],
+  };
+
   render() {
     return (
-      <div>
-        <h1>Hello from Create React Ap</h1>
-        <p>I am in a React Component!</p>
-      </div>
+      <ul>
+        { this.state.todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     );
   }
 }
