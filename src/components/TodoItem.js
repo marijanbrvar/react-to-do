add11dbbfb9d1eae83ad36f56d45b0ad725b38d9
Marijan Brvar <marijan.brvar@gmail.com>
2021-08-23 12:14:13 +0200

    Add lifecycle methods

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -12,6 +12,10 @@ class TodoItem extends React.Component {
     editing: false,
   }
 
+  componentWillUnmount() {
+    console.log('Cleaning up...');
+  }
+
   handleEditing = () => {
     this.setState({
       editing: true,
